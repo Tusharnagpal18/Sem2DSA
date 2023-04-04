01b87e863b0bc9860446d25ba3cd49819b31e78a
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-04-04 10:47:06 +0530

    Create sumArray.c

---
+++ sumArray.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+  int a[20],n,i,sum=0;
+  printf("Enter size of array\n");
+  scanf("%d",&n);
+  if(n>20)
+  {
+   printf("enter size <=20");
+  }
+  else{
+    for(i=0;i<n;i++){
+     scanf("%d",&a[i]);
+  }
+  for(i=0;i<n;i++){
+     sum+=a[i];
+  }
+  printf("sum = "+sum);
+  }
+ getch();
+}
