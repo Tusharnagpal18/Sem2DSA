fe4a8952f78147d39817562cc1c0a75e489b2eff
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-04-04 10:45:07 +0530

    Create minmax.c

---
+++ minmax.c
@@ -0,0 +1,22 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int a[20],min,max;
+   int n;
+   printf("enter the size of array\n");
+   scanf("%d",&n);
+   for(int i=0;i<n;i++){
+       scanf("%d",&a[i]);
+   }
+   max=min=a[0];
+   for(int i=0;i<n;i++){
+       if(a[i]>max){
+           max=a[i];
+       }
+       if(a[i]<min)
+         min=a[i];
+   }
+   printf("%d %d",max,min);
+    return 0;
+}
