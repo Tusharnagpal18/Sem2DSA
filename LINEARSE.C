14d1c4a8a086a968f06ccc576516c2b2d6826c4c
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-04-04 10:44:21 +0530

    Add files via upload

---
+++ LINEARSE.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+clrscr();
+int a[30];
+int n,i,m,flag=1;
+printf("enter the size of array\n");
+scanf("%d",&n);
+if(n>30){
+printf("enter size <= 30");
+}
+else{
+
+for(i=0;i<n;i++){
+scanf("%d",&a[i]);
+}
+printf("enter number to search\n");
+scanf("%d",&m);
+for(i=0;i<n;i++){
+ if(m==a[i]){
+  printf("found at %d index",i);
+  flag=0;
+  break;
+ }
+}
+if(flag==1)
+ printf("element not found!");
+}
+getch();
+}
---
+++ MATADD.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#inclue<conio.h>
+void main(){
+int a[10][10],b[10][10],c[10][10];
+int n,i,j;
+printf("Enter the size of array\n");
+scanf("%d",&n);
+
+for(i=0;i<n;i++){
+ for(j=0;j<n;j++){
+
+
+ }
+
+}
+}
