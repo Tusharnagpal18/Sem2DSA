7590306dd17c9f946c83c0a8a8b1abfd961abf7a
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-08-15 23:41:16 +0530

    Update MatrixAddition.c

--- MatrixAddition.c
+++ MatrixAddition.c
@@ -6,11 +6,13 @@ int a[10][10],b[10][10],c[10][10];
 int n,i,j;
 printf("Enter the size of array\n");
 scanf("%d",&n);
+    // input for first matrix
 for(i=0;i<n;i++){
  for(j=0;j<n;j++){
       scanf("%d",&a[i][j]);
     }
  }
+
  for(i=0;i<n;i++){
    for(j=0;j<n;j++){
       scanf("%d",&b[i][j]);
